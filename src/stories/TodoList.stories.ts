e697c5baffb544672ec708b80f7f34886de3c3dd
Kazu Kuroiwa <kazu.kuroiwa@nijibox.co.jp>
2023-08-27 16:00:40 +0900

    storyの作成

--- src/stories/TodoList.stories.ts
+++ src/stories/TodoList.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { TodoList } from "@/components/TodoList";
 import { todos } from "@/Todos";
+import { within, userEvent, fireEvent } from "@storybook/testing-library";
 
 const meta = {
   title: "TodoList",
@@ -19,9 +20,35 @@ export const Default: Story = {
   },
 };
 
+export const CheckTodo: Story = {
+  args: {
+    todos,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByLabelText("買い物に行く"));
+  },
+};
+
 export const AddTodo: Story = {
   args: {
     todos,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.type(canvas.getByRole("textbox"), "勉強する").then(() => {
+      userEvent.click(canvas.getByRole("button", { name: "Add Todo" }));
+    });
+  },
+};
+
+export const AddTodoWithoutText: Story = {
+  args: {
+    todos,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole("button", { name: "Add Todo" }));
   },
 };
 
@@ -29,10 +56,23 @@ export const EditTodo: Story = {
   args: {
     todos,
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const editButton = canvas.getAllByRole("button", { name: "Edit" })[0];
+    await userEvent.click(editButton);
+    const textbox = canvas.getByDisplayValue("買い物に行く");
+    fireEvent.change(textbox, { target: { value: "勉強する" } });
+    await userEvent.click(canvas.getByRole("button", { name: "Done" }));
+  },
 };
 
 export const DeleteTodo: Story = {
   args: {
     todos,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const deleteButton = canvas.getAllByRole("button", { name: "Delete" })[0];
+    await userEvent.click(deleteButton);
   },
 };
